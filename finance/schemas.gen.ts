3f6fa786dd68ee20fd9587609ce9f04ab7769281
shevchenko126 <43893100+shevchenko126@users.noreply.github.com>
2024-12-09 14:08:52 +0200

    fixed

--- finance/schemas.gen.ts
+++ finance/schemas.gen.ts
@@ -2,185 +2,6 @@
 
 export const CargoPaySchemaSchema = {
     properties: {
-        vin_code: {
-            anyOf: [
-                {
-                    type: 'string'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Vin Code'
-        },
-        cost: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Cost',
-            default: 0
-        },
-        sublot: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Sublot',
-            default: 0
-        },
-        auct_collect: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Auct Collect',
-            default: 0
-        },
-        penalty: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Penalty',
-            default: 0
-        },
-        insurance_pay: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Insurance Pay',
-            default: 0
-        },
-        sea_freight: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Sea Freight',
-            default: 0
-        },
-        land_delivery: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Land Delivery',
-            default: 0
-        },
-        broker_sevice_amount: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Broker Sevice Amount',
-            default: 0
-        },
-        storage_sevice_amount: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Storage Sevice Amount',
-            default: 0
-        },
-        doc_change_cost: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Doc Change Cost',
-            default: 0
-        },
-        closed_location_cost: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Closed Location Cost',
-            default: 0
-        },
-        total_amount: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Total Amount',
-            default: 0
-        },
-        paid_amount: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Paid Amount',
-            default: 0
-        },
-        left_to_pay_amount: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Left To Pay Amount',
-            default: '0.00'
-        },
         amount: {
             type: 'number',
             title: 'Amount'
@@ -360,18 +181,6 @@ export const CargoSchemaSchema = {
             title: 'Paid Amount',
             default: 0
         },
-        left_to_pay_amount: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Left To Pay Amount',
-            default: '0.00'
-        },
         id: {
             type: 'integer',
             title: 'Id'
@@ -387,6 +196,18 @@ export const CargoSchemaSchema = {
                 }
             ],
             title: 'Dealer Id'
+        },
+        left_to_pay_amount: {
+            anyOf: [
+                {
+                    type: 'number'
+                },
+                {
+                    type: 'null'
+                }
+            ],
+            title: 'Left To Pay Amount',
+            default: '0.00'
         }
     },
     type: 'object',
@@ -562,18 +383,6 @@ export const CargoUpdateSchemaSchema = {
             ],
             title: 'Paid Amount',
             default: 0
-        },
-        left_to_pay_amount: {
-            anyOf: [
-                {
-                    type: 'number'
-                },
-                {
-                    type: 'null'
-                }
-            ],
-            title: 'Left To Pay Amount',
-            default: '0.00'
         }
     },
     type: 'object',
@@ -639,12 +448,12 @@ export const PayCargoResponseSchema = {
 
 export const TransactionSchema = {
     properties: {
-        created_by: {
+        created_by_user_id: {
             type: 'string',
             format: 'uuid',
-            title: 'Created By'
+            title: 'Created By User Id'
         },
-        created_by_role: {
+        created_by_user_role: {
             '$ref': '#/components/schemas/UserRole'
         },
         user_id: {
@@ -697,7 +506,7 @@ export const TransactionSchema = {
         }
     },
     type: 'object',
-    required: ['created_by', 'created_by_role', 'user_id', 'amount', 'balance_before', 'balance_after', 'cargo_id', 'cargo', 'updated_on', 'created_on'],
+    required: ['created_by_user_id', 'created_by_user_role', 'user_id', 'amount', 'balance_before', 'balance_after', 'cargo_id', 'cargo', 'updated_on', 'created_on'],
     title: 'Transaction'
 } as const;
 
